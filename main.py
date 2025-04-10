4aa5131e2ae8f7f70290f3475f97fcf586490aab
coni1027 <nikolabitoria@gmail.com>
2025-04-10 21:22:38 +0800

    Polished movement and fixed tail mechanics for drawing

--- main.py
+++ main.py
@@ -4,16 +4,23 @@ from snake import Snake
 """CONSTANTS"""
 WIDTH = 900
 HEIGHT = 600
-cellSize = 20
+cellSize = 30
 startPos = (5,5)
+tail1 = (4,5)
+tail2 = (3,5)
+moveDelay = 50 # in ms
 
 def drawGrid():
-    
     for x in range(0, WIDTH, cellSize):
         for y in range(0, HEIGHT, cellSize):
             cell = pygame.Rect(x,y,cellSize,cellSize)
             pygame.draw.rect(screen,"white",cell,1)
 
+def drawHUD():
+    snakePos = font.render(f"pos: {snake.headPosX,snake.headPosY}",True,"cyan")
+    snakeLength = font.render(f"length: {snake.length}",True,"cyan")
+    screen.blit(snakePos,(40,40))
+    screen.blit(snakeLength,(40,60))
     
 pygame.init()
 pygame.display.set_caption("Snake")
@@ -23,10 +30,9 @@ clock = pygame.time.Clock()
 font = pygame.font.Font(None,25)
 
 
-snake = Snake(cellSize,startPos)
+snake = Snake(cellSize,startPos,[tail1,tail2])
 
-
-
+lastMoveTime = pygame.time.get_ticks()
 
 running = True
 while running:
@@ -37,14 +43,18 @@ while running:
 
     
     keys = pygame.key.get_pressed()
-    snakePos = font.render(f"pos: {snake.headPosX,snake.headPosY}",True,"cyan")
+    
     
     screen.fill("black")
     drawGrid()
-    screen.blit(snakePos,(40,40))
+    drawHUD()
     
     snake.draw(screen)
-    snake.move(keys,pygame.K_UP,pygame.K_DOWN,pygame.K_LEFT,pygame.K_RIGHT)
+    now = pygame.time.get_ticks()
+    if now - lastMoveTime > moveDelay:
+        snake.update()
+        lastMoveTime = now
+    snake.directionChange(keys,pygame.K_UP,pygame.K_DOWN,pygame.K_LEFT,pygame.K_RIGHT)
 
     pygame.display.update()
     clock.tick(60)
--- snake.py
+++ snake.py
@@ -10,19 +10,34 @@ class Snake():
         if segments == None:
             segments = []
         self.segments = segments
+        self.direction = (1,0)
+        self.length = len(self.segments)+1
 
-    def move(self,keylist:list,upkey,downkey,leftkey,rightkey):
-        if keylist[upkey]:
-            self.headPosY -= 1
-        if keylist[downkey]:
-            self.headPosY += 1
-        if keylist[leftkey]:
-            self.headPosX -= 1
-        if keylist[rightkey]:
-            self.headPosX += 1
+    def update(self):
+        self.segments.insert(0, (self.headPosX, self.headPosY))
 
-        self.headRect.topleft = (self.headPosX*self.size, self.headPosY*self.size)
-    
+        dx, dy = self.direction
+        self.headPosX += dx
+        self.headPosY += dy
+        self.headRect.topleft = (self.headPosX * self.size, self.headPosY * self.size)
+
+        # Keep the tail length same as size
+        if len(self.segments) > self.length-1:
+            self.segments.pop()
+
+
+    def directionChange(self,keylist:list,upkey,downkey,leftkey,rightkey):
+        currentDirection = self.direction
+
+        if keylist[upkey] and currentDirection != (0,1):
+            self.direction = (0,-1)
+        elif keylist[downkey] and currentDirection != (0,-1):
+            self.direction = (0,1)
+        elif keylist[leftkey] and currentDirection != (1,0):
+            self.direction = (-1,0)
+        elif keylist[rightkey] and currentDirection != (-1,0):
+            self.direction = (1,0)
+
     def draw(self,screen):
         pygame.draw.rect(screen, "green", self.headRect)
         for segment in self.segments:
